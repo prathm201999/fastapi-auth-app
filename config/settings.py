08451901dd24f72545ed496b803eac51098bc1d0
Prathmesh Rajendra Ingole <46246793+prathm201999@users.noreply.github.com>
2024-08-21 15:58:23 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str = "postgresql+asyncpg://authdb_owner:V97gEkaPchLf@ep-plain-water-a5scbycn.us-east-2.aws.neon.tech/authdb"
-    secret_key: str = "empty_string"
+    secret_key: str = "empty_string_1"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
     refresh_token_expire_days: int = 7
